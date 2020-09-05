41705c46a686ad11208bfd93decfa708f542dea6
martinpetrov000 <martin.petrov033@gmail.com>
2020-09-05 15:06:06 +0300

    Add routing

---
+++ GalleryClient/client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <router-view></router-view>
+    <nav>
+      <router-link to="/" exact>Home</router-link>
+      <router-link to="/login">Login</router-link>      
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
