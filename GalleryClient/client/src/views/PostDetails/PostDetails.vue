5dcdad87650d085dad896ce301a25ca97b7e2a8a
martinpetrov000 <martin.petrov033@gmail.com>
2020-10-18 17:03:55 +0300

    Finish like/unlike functionality

--- GalleryClient/client/src/views/PostDetails/PostDetails.vue
+++ GalleryClient/client/src/views/PostDetails/PostDetails.vue
@@ -5,13 +5,20 @@
       <div>
         <img v-if="state.post.pictureUrl" :src="state.post.pictureUrl" class="profile-icon" />
         <img v-else src="@/assets/icons/profile.png" class="profile-icon" />
-        <p>Created by: {{state.post.userName}}</p>
+        <p>Created by: {{state.post.userName}}</p>        
       </div>
       <div>
-        <img v-if="state.isLiked" class="heart-icon" src="@/assets/icons/heart-solid.svg" @click="like" alt="liked heart" />
+        <img
+          class="calendar-icon"
+          src="@/assets/icons/calendar-alt-solid.svg"
+          alt="calendar"
+        /><br>
+        <p>{{ state.createdOn }}</p>
+      </div>
+      <div>
+        <img v-if="state.isLiked" class="heart-icon" src="@/assets/icons/heart-solid.svg" @click="unlike" alt="liked heart" />
         <img v-else class="heart-icon" src="@/assets/icons/heart-regular.svg" @click="like" alt="like heart" />
-        <p>Likes: {{ state.post.likes }}</p>
-        {{ state.createdOn }}
+        <p>Likes: {{ state.likes }}</p>        
       </div>
     </div>
 
@@ -40,46 +47,48 @@ import { postService } from "../../services";
 export default defineComponent({
   setup() {
     const state = reactive({
-      post: [],      
-      createdOn: null,
+      post: [],
+      likes: 0,
       isLiked: false,
+      createdOn: null,      
       message: ''
     });
 
     watchEffect(async () => {
       const id = window.location.href.split("/")[3];
       const post = await postService.getById(id);
-      
-      state.post = post;
+
+      state.likes = post.likes;
+      state.isLiked = post.isLiked;
+      state.post = post;      
       state.createdOn = moment(post.createdOn, "YYYYMMDD").fromNow();
     });
 
-    const like = async() => {
-      //is it liked
-      if(state.isLiked === true) {
-        let status = await postService.unlike(localStorage.getItem('token'), state.post.id);
-      
-        if(status == 200) {
-          state.post.likes = state.post.likes - 1;
-          state.isLiked = false;
-        } else if(status == 400) {
-          state.message = "You had not liked this post.";
-        }
-      }
-
+    const like = async() => {      
       let status = await postService.like(localStorage.getItem('token'), state.post.id);
       
       if(status == 200) {
-        state.post.likes = state.post.likes + 1;
+        state.likes = state.post.likes + 1;
         state.isLiked = true;
       } else if(status == 400) {
         state.message = "You had already liked this post.";
       }
     }
+    
+    const unlike = async() => {
+      let status = await postService.unlike(localStorage.getItem('token'), state.post.id);      
+      if(status == 200) {
+        state.likes = state.likes - 1;          
+        state.isLiked = false;          
+      } else if(status == 400) {
+        state.message = "You had not liked this post.";
+      }
+    }
 
     return {
       state,
-      like
+      like,
+      unlike
     };
   }
 });
