2b5512a3d3c0f58d9e7568e9c794b8e9f2fa8507
martinpetrov000 <martin.petrov033@gmail.com>
2020-12-23 09:01:13 +0200

    Add dropdown header for mobile design

--- GalleryClient/client/src/components/core/Header.vue
+++ GalleryClient/client/src/components/core/Header.vue
@@ -14,9 +14,25 @@
       <router-link to="/">Home</router-link>
       <router-link to="/submit" v-if="isAuth()">Submit a photo</router-link>
       <router-link to="/users" v-if="isAuth()">Users</router-link>
+      <router-link to="/login" v-if="!isAuth()">Login</router-link>
       <a href="#" @click="logout" v-if="isAuth()">Logout</a>
-      <router-link to="/login" v-else>Login</router-link>
     </div>
+
+    <div v-if="state.showMenu" class="dropdown">
+      <li><router-link to="/" @click="toggleMenu">Home</router-link></li>
+      <li>
+        <router-link to="/submit" v-if="isAuth()" @click="toggleMenu">Submit a photo</router-link>
+      </li>
+      <li><router-link to="/users" v-if="isAuth()" @click="toggleMenu">Users</router-link></li>
+      <li><router-link to="/login" v-if="!isAuth()" @click="toggleMenu">Login</router-link></li>
+      <li><a href="#" @click="logout" v-if="isAuth()">Logout</a></li>
+    </div>
+    <img
+      src="@/assets/icons/bars-solid.svg"
+      alt="menu"
+      class="menu-icon"
+      @click="toggleMenu"
+    />
   </div>
 </template>
 
@@ -31,6 +47,7 @@ export default defineComponent({
   setup() {
     const state = reactive({
       pictureUrl: "",
+      showMenu: false,
     });
 
     watchEffect(async () => {
@@ -40,6 +57,10 @@ export default defineComponent({
       }
     });
 
+    function toggleMenu() {
+      state.showMenu = !state.showMenu;
+    }
+
     function isAuth() {
       return store.state.auth.state.isAuth;
     }
@@ -47,11 +68,12 @@ export default defineComponent({
     function logout() {
       store.state.auth.dispatch("logout");
 
-      router.push('/');
+      router.push("/");
     }
 
     return {
       state,
+      toggleMenu,
       isAuth,
       logout,
     };
@@ -60,7 +82,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-#nav {
+#nav {  
   display: flex;
   justify-content: center;
   padding: 2em;
@@ -75,6 +97,18 @@ export default defineComponent({
         color: #42b983;
       }
     }
+  }
+
+  .dropdown {
+    display: none;
+  }
+
+  .menu-icon {
+    display: none;
+    width: 2em;
+    height: 2em;
+    margin: 0 0.5em;
+    margin-bottom: -0.7em;
   }
 
   .profile-picture {
@@ -95,6 +129,48 @@ export default defineComponent({
         margin-left: 0;
       }
     }
-  }  
+  }
+
+  @media only screen and (max-width: 750px) {
+    #nav {
+      margin-bottom: 10em;
+    }
+
+    .links {
+      a {
+        display: none;
+      }
+    }
+
+    .dropdown {
+      display: block;
+      position: absolute;
+      top: 2em;
+      right: 2em;
+      margin: 3em 0;
+      width: 7em;
+      height: 6em;
+      padding: 2em;
+      list-style: none;
+      background-color: #111111;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+      z-index: 1;
+      border-radius: 10%;
+
+      a {
+        color: #fff;
+        text-decoration: none;
+        font-size: 1.2em;        
+        margin: 2em 0;
+      }
+
+      a:hover {color:#666;}
+    }
+
+    .menu-icon {
+      display: block;
+    }
+  }
 }
 </style>
