202ceb911d2e5b02dc57132ac896affe81c07f3a
martinpetrov000 <martin.petrov033@gmail.com>
2021-05-30 12:10:23 +0300

    Add collection name

---
+++ GalleryServer/GalleryServer/Data/Migrations/20210529105403_AddCollectionName.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GalleryServer.Data.Migrations
+{
+    public partial class AddCollectionName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Collection_CollectionId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Collection",
+                table: "Collection");
+
+            migrationBuilder.RenameTable(
+                name: "Collection",
+                newName: "Collections");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Collections",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Collections",
+                table: "Collections",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Collections_CollectionId",
+                table: "Posts",
+                column: "CollectionId",
+                principalTable: "Collections",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Collections_CollectionId",
+                table: "Posts");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Collections",
+                table: "Collections");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Collections");
+
+            migrationBuilder.RenameTable(
+                name: "Collections",
+                newName: "Collection");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Collection",
+                table: "Collection",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Collection_CollectionId",
+                table: "Posts",
+                column: "CollectionId",
+                principalTable: "Collection",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- GalleryServer/GalleryServer/Data/Models/Collection.cs
+++ GalleryServer/GalleryServer/Data/Models/Collection.cs
@@ -8,6 +8,8 @@
         [Key]
         public int Id { get; set; }
 
+        public string Name { get; set; }
+
         public virtual ICollection<Post> Posts { get; set; }
     }
 }
