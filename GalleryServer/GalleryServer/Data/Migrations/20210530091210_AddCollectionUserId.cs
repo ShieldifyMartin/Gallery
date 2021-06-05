9ea48f29ba21c5a211e1d339663b700750594506
martinpetrov000 <martin.petrov033@gmail.com>
2021-06-05 14:40:15 +0300

    Add user id in collection

---
+++ GalleryServer/GalleryServer/Data/Migrations/20210530091210_AddCollectionUserId.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GalleryServer.Data.Migrations
+{
+    public partial class AddCollectionUserId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Collections",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collections_UserId",
+                table: "Collections",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Collections_AspNetUsers_UserId",
+                table: "Collections",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Collections_AspNetUsers_UserId",
+                table: "Collections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Collections_UserId",
+                table: "Collections");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Collections");
+        }
+    }
+}
--- GalleryServer/GalleryServer/Data/Models/Collection.cs
+++ GalleryServer/GalleryServer/Data/Models/Collection.cs
@@ -10,6 +10,11 @@
 
         public string Name { get; set; }
 
+        [Required]
+        public string UserId { get; set; }
+
+        public virtual User User { get; set; }
+
         public virtual ICollection<Post> Posts { get; set; }
     }
 }
